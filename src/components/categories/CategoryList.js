1ae50be36d06be164a67cba6b02282f5ac48933a
YunusEmreNalbant <yunus.nalbant0@gmail.com>
2021-01-22 18:47:41 +0300

    connect ile baglamak

--- src/components/categories/CategoryList.js
+++ src/components/categories/CategoryList.js
@@ -1,11 +1,27 @@
 import React from 'react';
+import {connect} from "react-redux"
 
-const CategoryList = () => {
-    return (
-        <div>
-            <h3>Category</h3>
-        </div>
-    );
-};
 
-export default CategoryList;
+class CategoryList extends React.Component {
+    render() {
+        return (
+            <div>
+                <h3>Category</h3>
+                <h5>Seçili Kategori: {this.props.currentCategory.categoryName}</h5>
+            </div>
+        );
+    }
+}
+
+function mapStateToProps(state) {
+
+    return {
+        currentCategory: state.changeCategoryReducer
+    }
+}
+
+export default connect(mapStateToProps)(CategoryList);
+
+
+
+
